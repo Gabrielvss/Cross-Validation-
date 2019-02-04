fd09c222e13ac341f6a3bd1149069dd1c94328f5
Gabrielvss <45042131+Gabrielvss@users.noreply.github.com>
2019-02-04 14:02:54 -0200

    Update cross_val_stratifiedkfold.py

--- cross_val_stratifiedkfold.py
+++ cross_val_stratifiedkfold.py
@@ -1,3 +1,4 @@
+#preprocessing for data base
 import pandas as pd
 
 base = pd.read_csv('credit-data.csv')
@@ -15,8 +16,34 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 previsores = scaler.fit_transform(previsores)
 
-from sklearn.model_selection import cross_val_score
+#import the function
 from sklearn.naive_bayes import GaussianNB
-classificador = GaussianNB()
+
+
+import numpy as np
+
+b= np.zeros(shape=(previsores.shape[0], 1))
+
+#import cross validation with stratifiedkfold
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+kfold = StratifiedKFold(n_splits = 10, shuffle = True, random_state = 0)
+resultados = []
+
+for indice_treinamento, indice_teste in kfold.split(previsores,
+                                                    np.zeros(shape=(previsores.shape[0], 1))):
+    #look how to the function StratifiedKfold work 
+    #print('indice treinamento:', indice_treinamento, 'indice_teste: ', indice_teste)
+    
+    #training and results
+    classificador = GaussianNB()
+    classificador.fit(previsores[indice_treinamento],classe[indice_treinamento])
+    previsoes = classificador.predict(previsores[indice_teste])
+    precisao = accuracy_score(classe[indice_teste], previsoes)
+    resultados.append(precisao)
+
+#mean of results of each splits
+resultados = np.asarray(resultados)
+resultados.mean()
 
 
